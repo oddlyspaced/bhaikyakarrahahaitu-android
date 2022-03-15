311015b0bdd8dea94f0e3f98aee1f92a5c481579
oddlyspaced <oddlyspaced1@gmail.com>
2022-03-15 14:32:54 +0530

    src: AppListAdapter: use added initialChecked parameter to check items by default

--- app/src/main/java/com/oddlyspaced/bkkrht/AppListAdapter.kt
+++ app/src/main/java/com/oddlyspaced/bkkrht/AppListAdapter.kt
@@ -8,24 +8,30 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.oddlyspaced.bkkrht.databinding.ItemAppBinding
 
-class AppListAdapter(private val context: Context) : RecyclerView.Adapter<AppListAdapter.AppViewHolder>()  {
+class AppListAdapter(private val context: Context, initialChecked: ArrayList<String> = arrayListOf()) : RecyclerView.Adapter<AppListAdapter.AppViewHolder>()  {
 
     private val appList = arrayListOf<ApplicationInfo>()
+    private val checkedAppList = arrayListOf<Boolean>()
     private val packageManager: PackageManager = context.packageManager
 
     init {
         context.packageManager.getInstalledApplications(PackageManager.GET_META_DATA).forEach { info ->
             if (info.flags and ApplicationInfo.FLAG_SYSTEM == 0) {
                 appList.add(info)
+                checkedAppList.add(initialChecked.contains(info.packageName))
             }
         }
     }
 
     inner class AppViewHolder(private val item: ItemAppBinding) : RecyclerView.ViewHolder(item.root) {
-        fun bind(data: ApplicationInfo) {
+        fun bind(data: ApplicationInfo, position: Int) {
             item.txPkg.text = data.packageName
             item.txApp.text = data.loadLabel(packageManager)
             item.imgIcon.setImageDrawable(data.loadIcon(packageManager))
+            item.cbApp.isChecked = checkedAppList[position]
+            item.cbApp.setOnCheckedChangeListener { _, checked ->
+                checkedAppList[position] = checked
+            }
         }
     }
 
@@ -34,10 +40,20 @@ class AppListAdapter(private val context: Context) : RecyclerView.Adapter<AppLis
     }
 
     override fun onBindViewHolder(holder: AppViewHolder, position: Int) {
-        holder.bind(appList[position])
+        holder.bind(appList[position], position)
     }
 
     override fun getItemCount(): Int {
         return appList.size
+    }
+
+    fun getCheckedApps(): ArrayList<String> {
+        val checkedApps = arrayListOf<String>()
+        checkedAppList.forEachIndexed { index, checked ->
+            if (checked) {
+                checkedApps.add(appList[index].packageName)
+            }
+        }
+        return checkedApps
     }
 }
