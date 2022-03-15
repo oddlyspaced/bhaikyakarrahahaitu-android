ed6b84bc1a9b20f101d46bffe16b40c1911c1e4b
oddlyspaced <oddlyspaced1@gmail.com>
2022-03-15 14:28:57 +0530

    src: SharedPreferenceManager: setup tiny pref manager class with helper functions

---
+++ app/src/main/java/com/oddlyspaced/bkkrht/SharedPreferenceManager.kt
@@ -0,0 +1,24 @@
+package com.oddlyspaced.bkkrht
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+
+class SharedPreferenceManager(context: Context) {
+
+    private companion object {
+        const val APPS_KEY = "saved_apps"
+    }
+
+    private val sharedPreferences = context.getSharedPreferences("prefs", AppCompatActivity.MODE_PRIVATE)
+
+    fun saveAppsList(apps: ArrayList<String>) {
+        val editor = sharedPreferences.edit()
+        editor.putString(APPS_KEY, apps.joinToString("|"))
+        editor.apply()
+    }
+
+    fun readAppsList(): ArrayList<String> {
+        val raw = sharedPreferences.getString(APPS_KEY, "") ?: ""
+        return ArrayList(raw.split("|"))
+    }
+}
