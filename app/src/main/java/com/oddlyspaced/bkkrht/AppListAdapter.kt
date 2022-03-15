c657be1cf1fb88e84afc47a012c2d55613dbc56e
oddlyspaced <oddlyspaced1@gmail.com>
2022-03-15 12:02:39 +0530

    src: AppListAdapter: setup sample functioning app list adapter

--- app/src/main/java/com/oddlyspaced/bkkrht/AppListAdapter.kt
+++ app/src/main/java/com/oddlyspaced/bkkrht/AppListAdapter.kt
@@ -1,4 +1,43 @@
 package com.oddlyspaced.bkkrht
 
-class AppListAdapter {
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.oddlyspaced.bkkrht.databinding.ItemAppBinding
+
+class AppListAdapter(private val context: Context) : RecyclerView.Adapter<AppListAdapter.AppViewHolder>()  {
+
+    private val appList = arrayListOf<ApplicationInfo>()
+    private val packageManager: PackageManager = context.packageManager
+
+    init {
+        context.packageManager.getInstalledApplications(PackageManager.GET_META_DATA).forEach { info ->
+            if (info.flags and ApplicationInfo.FLAG_SYSTEM == 0) {
+                appList.add(info)
+            }
+        }
+    }
+
+    inner class AppViewHolder(private val item: ItemAppBinding) : RecyclerView.ViewHolder(item.root) {
+        fun bind(data: ApplicationInfo) {
+            item.txPkg.text = data.packageName
+            item.txApp.text = data.loadLabel(packageManager)
+            item.imgIcon.setImageDrawable(data.loadIcon(packageManager))
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AppViewHolder {
+        return AppViewHolder(ItemAppBinding.inflate(LayoutInflater.from(context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: AppViewHolder, position: Int) {
+        holder.bind(appList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return appList.size
+    }
 }
