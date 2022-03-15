6435c5553b033f5e94a1c066da414fcade9c7fdb
oddlyspaced <oddlyspaced1@gmail.com>
2022-03-15 09:46:40 +0530

    Init

---
+++ app/src/main/java/com/oddlyspaced/bkkrht/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oddlyspaced.bkkrht
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
