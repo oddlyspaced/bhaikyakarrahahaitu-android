5742428309b4266089b7b96c80e1dbc3bf88894b
oddlyspaced <oddlyspaced1@gmail.com>
2022-03-15 12:03:26 +0530

    src,res/layout: MainActivity: Setup simple test layout

--- app/src/main/java/com/oddlyspaced/bkkrht/MainActivity.kt
+++ app/src/main/java/com/oddlyspaced/bkkrht/MainActivity.kt
@@ -2,10 +2,21 @@ package com.oddlyspaced.bkkrht
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.oddlyspaced.bkkrht.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvApps.apply {
+            layoutManager = LinearLayoutManager(applicationContext)
+            adapter = AppListAdapter(applicationContext)
+        }
     }
 }
